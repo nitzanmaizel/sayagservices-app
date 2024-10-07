f9b94a7e9356161436288139b5bbb59e16be0000
Nitzan Maizel <nitzanmaizel@gmail.com>
2024-10-08 01:32:04 +0300

    install react-query and add it to main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,8 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
 import { ThemeProvider } from '@mui/material/styles';
 import { CssBaseline } from '@mui/material';
 
@@ -12,21 +14,25 @@ import App from './App.tsx';
 import theme from './theme.tsx';
 import './index.css';
 
+const queryClient = new QueryClient();
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Router>
-      <SnackbarProvider>
-        <UserProvider>
-          <DocsProvider>
-            <ProductProvider>
-              <ThemeProvider theme={theme}>
-                <CssBaseline />
-                <App />
-              </ThemeProvider>
-            </ProductProvider>
-          </DocsProvider>
-        </UserProvider>
-      </SnackbarProvider>
+      <QueryClientProvider client={queryClient}>
+        <SnackbarProvider>
+          <UserProvider>
+            <DocsProvider>
+              <ProductProvider>
+                <ThemeProvider theme={theme}>
+                  <CssBaseline />
+                  <App />
+                </ThemeProvider>
+              </ProductProvider>
+            </DocsProvider>
+          </UserProvider>
+        </SnackbarProvider>
+      </QueryClientProvider>
     </Router>
   </StrictMode>
 );

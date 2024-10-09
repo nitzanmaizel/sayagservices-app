521175caaed981b61ba7ba6fbd4022063b11f71b
Nitzan Maizel <nitzanmaizel@gmail.com>
2024-10-09 22:46:35 +0300

    iconWrapper - add prop for onClick event

--- src/components/IconWrapper/IconWrapper.tsx
+++ src/components/IconWrapper/IconWrapper.tsx
@@ -1,14 +1,25 @@
 import React from 'react';
-import { Box, SvgIconProps } from '@mui/material';
+import { Box, Button, SvgIconProps } from '@mui/material';
 import { IconWrapperProps, mapTypeToIcon } from './mapTypeToIcon';
 
 const IconWrapper: React.FC<IconWrapperProps> = (props) => {
-  const { type, isDark, isDisabled, sx, ...otherProps } = props;
+  const { type, isDark, isDisabled, sx, onClick, ...otherProps } = props;
 
   const iconSx = {
     color: isDark ? '#333' : isDisabled ? '#999' : '#000',
     ...sx,
   };
+
+  if (typeof onClick === 'function' && !isDisabled) {
+    return (
+      <Button
+        onClick={onClick as unknown as React.MouseEventHandler<HTMLButtonElement>}
+        sx={{ minWidth: '20px', display: 'flex', justifyContent: 'center', alignItems: 'center' }}
+      >
+        {mapTypeToIcon[type]({ ...otherProps, sx: iconSx } as SvgIconProps)}
+      </Button>
+    );
+  }
 
   return mapTypeToIcon[type] ? (
     <Box sx={{ width: 30, display: 'flex', justifyContent: 'center', alignItems: 'center' }}>

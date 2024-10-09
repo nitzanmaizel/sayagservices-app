b2cdf30fb33d434ee4e69cf735ff25675242cddb
Nitzan Maizel <nitzanmaizel@gmail.com>
2024-10-09 22:41:41 +0300

    add tanstack devtools

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 import { ThemeProvider } from '@mui/material/styles';
 import { CssBaseline } from '@mui/material';
@@ -27,6 +28,7 @@ createRoot(document.getElementById('root')!).render(
                 <ThemeProvider theme={theme}>
                   <CssBaseline />
                   <App />
+                  <ReactQueryDevtools initialIsOpen={false} />
                 </ThemeProvider>
               </ProductProvider>
             </DocsProvider>

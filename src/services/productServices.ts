f2226017e417ad8338898e598740aa01e9ce3631
Nitzan Maizel <nitzanmaizel@gmail.com>
2024-10-14 02:59:26 +0300

    create useUpdateProductMutation for updating product

--- src/services/productServices.ts
+++ src/services/productServices.ts
@@ -1,6 +1,9 @@
-import { useQuery } from '@tanstack/react-query';
+import { QueryClient, useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
 import fetchAPI from './apiServices';
-import { RIProductsType } from '../types/ProductTypes';
+import { RIProductsType, IProduct } from '../types/ProductTypes';
+import { useSnackbar } from '../hooks/useSnackbar';
+
+const emptyProductsData: RIProductsType = { products: [], total: 0, limit: 0, page: 0 };
 
 export function useProductsQuery() {
   return useQuery({
@@ -10,4 +13,67 @@ export function useProductsQuery() {
       return { products, total, limit, page };
     },
   });
+}
+
+export function useUpdateProductMutation() {
+  const { showSnackbar } = useSnackbar();
+  const queryClient = useQueryClient();
+
+  return useMutation<
+    IProduct,
+    Error,
+    { productId: string; formData: FormData },
+    { previousData: RIProductsType }
+  >({
+    mutationFn: async ({ productId, formData }) => {
+      console.log({ productId, formData });
+
+      return await fetchAPI<IProduct>(`/products/${productId}`, {
+        method: 'PUT',
+        body: formData,
+      });
+    },
+    onMutate: async ({ productId, formData }) => {
+      const { previousData, previousProducts } = await cancelAndGetPreviousProductsData(
+        queryClient
+      );
+
+      const updatedProductData: Partial<IProduct> = { _id: productId };
+      formData.forEach((value, key) => {
+        updatedProductData[key as keyof IProduct] = value as any;
+      });
+
+      queryClient.setQueryData(['products'], {
+        ...previousData,
+        products: previousProducts.map((product) =>
+          product._id === productId ? { ...product, ...updatedProductData } : product
+        ),
+      });
+
+      return { previousData };
+    },
+    onError: (_error, _updatedProduct, context) => {
+      queryClient.setQueryData(['products'], context?.previousData);
+      showSnackbar('Error updating product', 'error');
+    },
+    onSuccess: (updatedProduct, _variables, context) => {
+      const previousData = context?.previousData;
+      if (previousData) {
+        const updatedProducts = previousData.products.map((product) =>
+          product._id === updatedProduct._id ? updatedProduct : product
+        );
+        queryClient.setQueryData(['products'], { ...previousData, products: updatedProducts });
+      }
+      showSnackbar('Product edited successfully');
+    },
+  });
+}
+
+async function cancelAndGetPreviousProductsData(
+  queryClient: QueryClient
+): Promise<{ previousData: RIProductsType; previousProducts: IProduct[] }> {
+  await queryClient.cancelQueries({ queryKey: ['products'] });
+  const previousData = queryClient.getQueryData<RIProductsType>(['products']) || emptyProductsData;
+  const previousProducts = previousData.products;
+  return { previousData, previousProducts };
 }

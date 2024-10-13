2c97346452fbaad2d9c7211ee03b844007544d2e
Nitzan Maizel <nitzanmaizel@gmail.com>
2024-10-14 02:56:22 +0300

    productType - add image props as file for new and updated products

--- src/types/ProductTypes.ts
+++ src/types/ProductTypes.ts
@@ -1,11 +1,12 @@
 export interface IProduct {
-  _id: string;
+  _id?: string;
   name: string;
   description: string;
   price: number;
   imageUrl?: string;
-  createdAt: string;
-  updatedAt: string;
+  createdAt?: string;
+  updatedAt?: string;
+  image?: File | null;
   __v: number;
 }
 

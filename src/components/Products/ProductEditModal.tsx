5ac17bd30d40185b53fcc7768ba73aaa534175bd
Nitzan Maizel <nitzanmaizel@gmail.com>
2024-10-14 02:58:35 +0300

    create Edit Product modal that will handle as a local state all the relevant stuff for editing product

--- src/components/Products/ProductEditModal.tsx
+++ src/components/Products/ProductEditModal.tsx
@@ -1,114 +1,124 @@
-import React from 'react';
+import React, { ChangeEvent, FormEvent } from 'react';
 import { Dialog, DialogTitle, DialogActions, DialogContent } from '@mui/material';
-import { TextField, Button, CardMedia, Input, InputLabel } from '@mui/material';
+import { TextField, Button, Box } from '@mui/material';
+
+import IconWrapper from '../IconWrapper/IconWrapper';
+import { isValidNumber } from '../../utils/date';
 import { IProduct } from '../../types/ProductTypes';
+import { useUpdateProductMutation } from '../../services/productServices';
 
 interface ProductEditModalProps {
-  isOpen: boolean;
-  editedProduct: Partial<IProduct>;
-  handleClose: () => void;
-  handleChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
-  handleSave: () => void;
+  productToEdit: Partial<IProduct>;
 }
 
-const ProductEditModal = ({
-  isOpen,
-  editedProduct,
-  handleClose,
-  handleChange,
-  handleSave,
-}: ProductEditModalProps) => {
-  const [imageFile, setImageFile] = React.useState<File | null>(null);
-  const [imagePreviewUrl, setImagePreviewUrl] = React.useState<string | undefined>(
-    editedProduct.imageUrl
-  );
+const ProductEditModal = ({ productToEdit }: ProductEditModalProps) => {
+  const productUpdateMutation = useUpdateProductMutation();
 
-  const handleImageChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const file = e.target.files?.[0];
+  const [product, setProduct] = React.useState<Partial<IProduct>>(productToEdit);
+  const [imagePreview, setImagePreview] = React.useState<string | undefined>(product?.imageUrl);
+  const [isOpen, setIsOpen] = React.useState(false);
 
-    if (file) {
-      setImageFile(file);
-      const previewUrl = URL.createObjectURL(file);
-      setImagePreviewUrl(previewUrl);
+  React.useEffect(() => {
+    return () => {
+      if (imagePreview) URL.revokeObjectURL(imagePreview);
+    };
+  }, [imagePreview]);
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+    const { name, value } = e.target;
+
+    if (name === 'image') {
+      const target = e.target as HTMLInputElement;
+      if (target.files && target.files[0]) {
+        const selectedImage = target.files[0];
+        setProduct((prev) => ({ ...prev, image: selectedImage }));
+
+        const previewURL = URL.createObjectURL(selectedImage);
+        setImagePreview(previewURL);
+      }
+    } else if (name === 'price') {
+      const numericValue = value === '' ? '' : Number(value);
+      if (isValidNumber(numericValue)) {
+        setProduct((prev) => ({ ...prev, price: numericValue }));
+      }
+    } else {
+      setProduct((prev) => ({ ...prev, [name]: value }));
     }
   };
 
-  const onSave = () => {
-    console.log('Edited Product:', editedProduct);
-    console.log('Selected Image File:', imageFile);
+  const handleSubmit = (e: FormEvent) => {
+    e.preventDefault();
+    if (!product.name || !product.description || !product.price) return;
 
-    if (imageFile && imagePreviewUrl?.startsWith('blob:')) {
-      URL.revokeObjectURL(imagePreviewUrl);
+    const formData = new FormData();
+    formData.append('name', product.name);
+    formData.append('description', product.description);
+    formData.append('price', product.price.toString());
+
+    if (product.image) {
+      formData.append('image', product.image);
     }
 
-    handleSave();
+    productUpdateMutation.mutate({ productId: product._id!, formData });
+
+    setIsOpen(false);
   };
 
   return (
-    <Dialog open={isOpen} onClose={handleClose} fullWidth maxWidth='sm'>
-      <DialogTitle>Edit Product</DialogTitle>
-      <DialogContent>
-        <TextField
-          margin='dense'
-          label='Name'
-          name='name'
-          value={editedProduct.name}
-          onChange={handleChange}
-          fullWidth
-        />
-        <TextField
-          margin='dense'
-          label='Description'
-          name='description'
-          value={editedProduct.description}
-          onChange={handleChange}
-          multiline
-          rows={4}
-          fullWidth
-        />
-        <TextField
-          margin='dense'
-          label='Price'
-          name='price'
-          value={editedProduct.price}
-          onChange={handleChange}
-          type='number'
-          fullWidth
-        />
-        <TextField
-          margin='dense'
-          label='Image URL'
-          name='imageUrl'
-          value={editedProduct.imageUrl}
-          onChange={handleChange}
-          fullWidth
-          disabled
-        />
-        <InputLabel sx={{ mt: 2 }}>Upload New Image</InputLabel>
-        <Input
-          type='file'
-          inputProps={{ accept: 'image/*' }}
-          onChange={handleImageChange}
-          fullWidth
-        />
-
-        {imagePreviewUrl && (
-          <CardMedia
-            component='img'
-            height='200'
-            image={imagePreviewUrl}
-            alt='Image Preview'
-            sx={{ borderRadius: 1, mt: 2 }}
+    <React.Fragment>
+      <IconWrapper type='edit' onClick={() => setIsOpen(true)} />
+      <Dialog open={isOpen} onClose={() => setIsOpen(false)} fullWidth maxWidth='sm'>
+        <DialogTitle>Edit Product</DialogTitle>
+        <DialogContent>
+          <TextField
+            margin='dense'
+            label='Name'
+            name='name'
+            value={product.name}
+            onChange={handleChange}
+            fullWidth
           />
-        )}
-      </DialogContent>
-      <DialogActions>
-        <Button onClick={handleClose}>Cancel</Button>
-        <Button onClick={onSave} variant='contained' color='primary'>
-          Save
-        </Button>
-      </DialogActions>
-    </Dialog>
+          <TextField
+            margin='dense'
+            label='Description'
+            name='description'
+            value={product.description}
+            onChange={handleChange}
+            multiline
+            rows={4}
+            fullWidth
+          />
+          <TextField
+            margin='dense'
+            label='Price'
+            name='price'
+            value={product.price}
+            onChange={handleChange}
+            type='number'
+            fullWidth
+          />
+          <Button variant='outlined' color='primary' component='label'>
+            Upload Image
+            <input type='file' hidden accept='image/*' name='image' onChange={handleChange} />
+          </Button>
+          {imagePreview && (
+            <Box sx={{ textAlign: 'center' }}>
+              <img
+                src={imagePreview}
+                alt='Image Preview'
+                style={{ maxWidth: '100%', maxHeight: 300 }}
+              />
+            </Box>
+          )}
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={() => setIsOpen(false)}>Cancel</Button>
+          <Button onClick={handleSubmit} variant='contained' color='primary'>
+            Save
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </React.Fragment>
   );
 };
 

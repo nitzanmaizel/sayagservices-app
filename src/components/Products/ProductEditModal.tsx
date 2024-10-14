11f1457c14894b73d351895269a7084cf8989705
Nitzan Maizel <nitzanmaizel@gmail.com>
2024-10-14 15:30:09 +0300

    finalize edit product modal

--- src/components/Products/ProductEditModal.tsx
+++ src/components/Products/ProductEditModal.tsx
@@ -3,20 +3,24 @@ import { Dialog, DialogTitle, DialogActions, DialogContent } from '@mui/material
 import { TextField, Button, Box } from '@mui/material';
 
 import IconWrapper from '../IconWrapper/IconWrapper';
-import { isValidNumber } from '../../utils/date';
 import { IProduct } from '../../types/ProductTypes';
+import { useSnackbar } from '../../hooks/useSnackbar';
 import { useUpdateProductMutation } from '../../services/productServices';
+import { isValidNumber } from '../../utils/date';
 
 interface ProductEditModalProps {
   productToEdit: Partial<IProduct>;
 }
 
 const ProductEditModal = ({ productToEdit }: ProductEditModalProps) => {
+  const { showSnackbar } = useSnackbar();
   const productUpdateMutation = useUpdateProductMutation();
 
   const [product, setProduct] = React.useState<Partial<IProduct>>(productToEdit);
   const [imagePreview, setImagePreview] = React.useState<string | undefined>(product?.imageUrl);
   const [isOpen, setIsOpen] = React.useState(false);
+
+  const modalFields = getFields(product as IProduct);
 
   React.useEffect(() => {
     return () => {
@@ -24,25 +28,36 @@ const ProductEditModal = ({ productToEdit }: ProductEditModalProps) => {
     };
   }, [imagePreview]);
 
-  const handleChange = (e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+  const renderImageUpload = () => (
+    <React.Fragment>
+      <Button variant='outlined' color='primary' component='label'>
+        Upload Image
+        <input type='file' hidden accept='image/*' name='image' onChange={handleImageChange} />
+      </Button>
+      {imagePreview && (
+        <Box sx={{ textAlign: 'center' }}>
+          <img
+            src={imagePreview}
+            alt='Image Preview'
+            style={{ maxWidth: '100%', maxHeight: 300 }}
+          />
+        </Box>
+      )}
+    </React.Fragment>
+  );
+
+  const handleChange = (e: ChangeEvent<HTMLTextAreaElement>) => {
     const { name, value } = e.target;
+    setProduct((prev) => ({ ...prev, [name]: value }));
+  };
 
-    if (name === 'image') {
-      const target = e.target as HTMLInputElement;
-      if (target.files && target.files[0]) {
-        const selectedImage = target.files[0];
-        setProduct((prev) => ({ ...prev, image: selectedImage }));
-
-        const previewURL = URL.createObjectURL(selectedImage);
-        setImagePreview(previewURL);
-      }
-    } else if (name === 'price') {
-      const numericValue = value === '' ? '' : Number(value);
-      if (isValidNumber(numericValue)) {
-        setProduct((prev) => ({ ...prev, price: numericValue }));
-      }
-    } else {
-      setProduct((prev) => ({ ...prev, [name]: value }));
+  const handleImageChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const target = e.target as HTMLInputElement;
+    if (target.files && target.files[0]) {
+      const selectedImage = target.files[0];
+      setProduct((prev) => ({ ...prev, image: selectedImage }));
+      const previewURL = URL.createObjectURL(selectedImage);
+      setImagePreview(previewURL);
     }
   };
 
@@ -50,17 +65,18 @@ const ProductEditModal = ({ productToEdit }: ProductEditModalProps) => {
     e.preventDefault();
     if (!product.name || !product.description || !product.price) return;
 
+    if (!isValidNumber(product.price)) {
+      showSnackbar('Price must be a valid number', 'error');
+      return;
+    }
+
     const formData = new FormData();
     formData.append('name', product.name);
     formData.append('description', product.description);
     formData.append('price', product.price.toString());
-
-    if (product.image) {
-      formData.append('image', product.image);
-    }
+    if (product.image) formData.append('image', product.image);
 
     productUpdateMutation.mutate({ productId: product._id!, formData });
-
     setIsOpen(false);
   };
 
@@ -70,46 +86,19 @@ const ProductEditModal = ({ productToEdit }: ProductEditModalProps) => {
       <Dialog open={isOpen} onClose={() => setIsOpen(false)} fullWidth maxWidth='sm'>
         <DialogTitle>Edit Product</DialogTitle>
         <DialogContent>
-          <TextField
-            margin='dense'
-            label='Name'
-            name='name'
-            value={product.name}
-            onChange={handleChange}
-            fullWidth
-          />
-          <TextField
-            margin='dense'
-            label='Description'
-            name='description'
-            value={product.description}
-            onChange={handleChange}
-            multiline
-            rows={4}
-            fullWidth
-          />
-          <TextField
-            margin='dense'
-            label='Price'
-            name='price'
-            value={product.price}
-            onChange={handleChange}
-            type='number'
-            fullWidth
-          />
-          <Button variant='outlined' color='primary' component='label'>
-            Upload Image
-            <input type='file' hidden accept='image/*' name='image' onChange={handleChange} />
-          </Button>
-          {imagePreview && (
-            <Box sx={{ textAlign: 'center' }}>
-              <img
-                src={imagePreview}
-                alt='Image Preview'
-                style={{ maxWidth: '100%', maxHeight: 300 }}
-              />
-            </Box>
-          )}
+          {modalFields.map(({ name, value, label, ...rest }) => (
+            <TextField
+              key={name}
+              name={name}
+              value={value}
+              label={label}
+              onChange={handleChange}
+              fullWidth
+              margin='dense'
+              {...rest}
+            />
+          ))}
+          {renderImageUpload()}
         </DialogContent>
         <DialogActions>
           <Button onClick={() => setIsOpen(false)}>Cancel</Button>
@@ -123,3 +112,42 @@ const ProductEditModal = ({ productToEdit }: ProductEditModalProps) => {
 };
 
 export default ProductEditModal;
+
+interface FieldConfig {
+  label: string;
+  name: string;
+  value: string | number;
+  type: string;
+  multiline?: boolean;
+  rows?: number;
+  required?: boolean;
+  error?: boolean;
+  helperText?: string;
+  [key: string]: any;
+}
+
+const getFields = (product: IProduct): FieldConfig[] => [
+  {
+    label: 'Name',
+    name: 'name',
+    value: product.name || '',
+    multiline: false,
+    type: 'text',
+    required: true,
+  },
+  {
+    label: 'Description',
+    name: 'description',
+    value: product.description || '',
+    multiline: true,
+    rows: 4,
+    type: 'text',
+  },
+  {
+    label: 'Price',
+    name: 'price',
+    value: product.price || '',
+    multiline: false,
+    type: 'text',
+  },
+];

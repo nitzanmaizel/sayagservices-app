8832d589ff504702c3faeb26ae7eb56f216d30bb
Nitzan Maizel <nitzanmaizel@gmail.com>
2024-10-14 15:49:01 +0300

    render the create product modal

--- src/pages/ProductsPage.tsx
+++ src/pages/ProductsPage.tsx
@@ -1,7 +1,8 @@
-import { Box, Button, Typography } from '@mui/material';
+import { Box, Typography } from '@mui/material';
 import PageWrapper from './PageWrapper';
 import ProductsList from '../components/Products/ProductsList';
 import { useUser } from '../hooks/useUser';
+import ProductCreateModal from '../components/Products/ProductCreateModal';
 
 const ProductsPage = () => {
   const { userInfo } = useUser();
@@ -9,15 +10,7 @@ const ProductsPage = () => {
     <PageWrapper>
       <Box mt={3} display={'flex'} justifyContent={userInfo?.isAdmin ? 'space-between' : 'center'}>
         <Typography variant='h4'>Products List</Typography>
-        {userInfo?.isAdmin && (
-          <Button
-            sx={{ height: 40, alignSelf: 'center', textTransform: 'capitalize' }}
-            variant='contained'
-            color='primary'
-          >
-            Add Product
-          </Button>
-        )}
+        {userInfo?.isAdmin && <ProductCreateModal />}
       </Box>
       <ProductsList />
     </PageWrapper>

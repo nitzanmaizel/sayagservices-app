daacca1a04ae2c423e7642bd6b7d24ac504f4247
Nitzan Maizel <nitzanmaizel@gmail.com>
2024-10-14 21:02:19 +0300

    update sidebar to match RTL

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -7,15 +7,6 @@ import { List, ListItem, ListItemButton, ListItemIcon, ListItemText } from '@mui
 import { useUser } from '../hooks/useUser';
 import IconWrapper from './IconWrapper/IconWrapper';
 
-const SidebarList = [
-  { id: '1', text: 'Home', icon: 'home', link: '/' },
-  { id: '2', text: 'Dashboard', icon: 'dashboard', link: '/dashboard' },
-  { id: '3', text: 'Create Doc', icon: 'addDoc', link: '/create' },
-  { id: '4', text: 'Recent Docs', icon: 'doc', link: '/recent' },
-  { id: '5', text: 'Search Doc', icon: 'searchDoc', link: '/search' },
-  { id: '6', text: 'About', icon: 'info', link: '/about' },
-];
-
 const Sidebar: React.FC = () => {
   const [open, setOpen] = React.useState(false);
   const { userInfo, login, logout } = useUser();
@@ -27,25 +18,25 @@ const Sidebar: React.FC = () => {
   };
 
   const DrawerList = (
-    <Box sx={{ width: 250 }} role='presentation' onClick={toggleDrawer(false)}>
+    <Box role='presentation' onClick={toggleDrawer(false)}>
       <List>
         {SidebarList.map(({ id, text, icon, link }) => (
           <ListItem key={id} disablePadding sx={{ margin: 0 }}>
             <ListItemButton onClick={() => navigate(link)}>
-              <ListItemText primary={text} />
               <ListItemIcon sx={{ justifyContent: 'center' }}>
                 <IconWrapper type={icon} />
               </ListItemIcon>
+              <ListItemText primary={text} />
             </ListItemButton>
           </ListItem>
         ))}
         <Divider />
         <ListItem key='logout' disablePadding>
           <ListItemButton onClick={logout}>
-            <ListItemText primary='Logout' />
             <ListItemIcon sx={{ justifyContent: 'center' }}>
               <IconWrapper type='logout' />
             </ListItemIcon>
+            <ListItemText primary='התנתקות' />
           </ListItemButton>
         </ListItem>
       </List>
@@ -57,10 +48,10 @@ const Sidebar: React.FC = () => {
       <List>
         <ListItem key='login' disablePadding>
           <ListItemButton onClick={login}>
-            <ListItemText primary='Login' />
             <ListItemIcon sx={{ justifyContent: 'center' }}>
               <IconWrapper type='login' />
             </ListItemIcon>
+            <ListItemText primary='התחברות' />
           </ListItemButton>
         </ListItem>
       </List>
@@ -68,15 +59,24 @@ const Sidebar: React.FC = () => {
   );
 
   return (
-    <>
+    <React.Fragment>
       <Button onClick={toggleDrawer(true)}>
         <IconWrapper type='menu' fontSize='large' sx={{ color: '#fff' }} />
       </Button>
-      <Drawer anchor='right' open={open} onClose={toggleDrawer(false)}>
+      <Drawer anchor='left' open={open} onClose={toggleDrawer(false)}>
         {userInfo ? DrawerList : LoginDrawer}
       </Drawer>
-    </>
+    </React.Fragment>
   );
 };
 
 export default Sidebar;
+
+const SidebarList = [
+  { id: '1', text: 'בית', icon: 'home', link: '/' },
+  { id: '2', text: 'מרכז שליטה', icon: 'dashboard', link: '/dashboard' },
+  { id: '3', text: 'יצירת מסמך', icon: 'addDoc', link: '/create' },
+  { id: '4', text: 'מסמכים אחרונים', icon: 'doc', link: '/recent' },
+  { id: '5', text: 'חיפוש מסמך', icon: 'searchDoc', link: '/search' },
+  { id: '6', text: 'אודות', icon: 'info', link: '/about' },
+];

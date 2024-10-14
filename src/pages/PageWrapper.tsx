e21cc5f42cc7720794decc4f6229ac51ffb9eef8
Nitzan Maizel <nitzanmaizel@gmail.com>
2024-10-14 15:47:51 +0300

    add new prop 'title' for rending the page title

--- src/pages/PageWrapper.tsx
+++ src/pages/PageWrapper.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
-import { Container } from '@mui/material';
+import { Container, Typography } from '@mui/material';
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
 
-const PageWrapper: React.FC<{ children: React.ReactNode }> = ({ children }) => {
+interface PageWrapperProps {
+  children: React.ReactNode;
+  title?: string;
+}
+
+const PageWrapper = ({ children, title }: PageWrapperProps) => {
   return (
     <div>
       <Navbar />
@@ -15,6 +20,11 @@ const PageWrapper: React.FC<{ children: React.ReactNode }> = ({ children }) => {
           width: { xs: '100%', md: '100%' },
         }}
       >
+        {title && (
+          <Typography variant='h4' align='center' mt={3} mb={3}>
+            {title}
+          </Typography>
+        )}
         {children}
       </Container>
       <Footer />

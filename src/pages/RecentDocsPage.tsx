c34d37938399e88f3926417e70567e55a310e57d
Nitzan Maizel <nitzanmaizel@gmail.com>
2024-10-15 15:22:24 +0300

    RecentDocs - render Skeleton when state isLoading

--- src/pages/RecentDocsPage.tsx
+++ src/pages/RecentDocsPage.tsx
@@ -1,19 +1,35 @@
 import React from 'react';
-import { CircularProgress } from '@mui/material';
-import { useDocs } from '../hooks/useDocs';
+import { ImageList, ImageListItem, useMediaQuery } from '@mui/material';
 import PageWrapper from './PageWrapper';
 import DocsList from '../components/DocList';
+import DocSkeleton from '../components/Skeletons/DocSkeleton';
+import { useDocs } from '../hooks/useDocs';
 
 const RecentDocsPage = () => {
   const { recentDocs, loading, getRecentDocs } = useDocs();
+  const matches = useMediaQuery('(min-width:600px)');
 
   React.useEffect(() => {
     getRecentDocs();
   }, [getRecentDocs]);
 
+  if (loading) {
+    return (
+      <PageWrapper title='מסמכים אחרונים'>
+        <ImageList cols={matches ? 3 : 1} gap={20} sx={{ width: '100%' }}>
+          {Array.from({ length: 3 }).map((_, index) => (
+            <ImageListItem key={index}>
+              <DocSkeleton />
+            </ImageListItem>
+          ))}
+        </ImageList>
+      </PageWrapper>
+    );
+  }
+
   return (
     <PageWrapper title='מסמכים אחרונים'>
-      {loading ? <CircularProgress /> : <DocsList documents={recentDocs} />}
+      <DocsList documents={recentDocs} />
     </PageWrapper>
   );
 };
